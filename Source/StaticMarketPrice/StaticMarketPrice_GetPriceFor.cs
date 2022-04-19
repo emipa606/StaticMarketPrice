0dd076a60a23f227dd5759474d6e20749d87a19c
Mlie <emipa606@users.noreply.github.com>
2022-04-19 21:06:43 +0200

    Fixed the issue with inverted relation impact when gifting silver.

--- Source/StaticMarketPrice/StaticMarketPrice_GetPriceFor.cs
+++ Source/StaticMarketPrice/StaticMarketPrice_GetPriceFor.cs
@@ -8,6 +8,11 @@ public static class StaticMarketPrice_GetPriceFor
 {
     public static bool Prefix(ref float __result, ref Tradeable __instance, TradeAction action)
     {
+        if (__instance.IsCurrency)
+        {
+            return true;
+        }
+
         __result = __instance.BaseMarketValue *
                    TradeSession.trader.TraderKind.PriceTypeFor(__instance.ThingDef, action).PriceMultiplier();
         return false;
